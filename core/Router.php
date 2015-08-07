15c24a0122c0fe0b4295e1f6ea25877ac23a5fd3
Yasunari Kondo <kyasunari123v@gmail.com>
2015-08-08 01:42:43 +0900

    resolveメソッドの実装
    マッチングを行う

--- core/Router.php
+++ core/Router.php
@@ -26,5 +26,24 @@ class Router
             $pattern = '/' . implode('/', $tokens);
             $routes[$pattern] = $params;
         }
+        
+        return $routes;
+    }
+    
+    public function resolve($path_info)
+    {
+        if('/' !== substr($path_info, 0, 1)) {
+            $path_info = '/' . $path_info;
+        }
+        
+        foreach ($this->routers as $pattern => $params) {
+            if(preg_match('#^' . $pattern . '$#', $path_info, $matches)) {
+                $params = array_merge($params, $matches);
+                
+                return $params;
+            }
+        }
+        
+        return false;
     }
 }
