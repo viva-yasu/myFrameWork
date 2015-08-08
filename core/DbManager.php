dec269813325a2a7fe767b6c9673d1a16e598828
Yasunari Kondo <kyasunari123v@gmail.com>
2015-08-08 09:58:24 +0900

    DbManagerクラスの実装
    接続情報に関するconnectメソッド、getConnectionメソッドの実装

---
+++ core/DbManager.php
@@ -0,0 +1,35 @@
+<?php
+
+class DbManager
+{
+    protected $connections = array();
+    
+    public function connect($name, $params)
+    {
+        $params = array_merge(array(
+            'dsn' => null,
+            'user' => '',
+            'password' => '',
+            'options' => array(),
+        ), $params);
+        
+        $con = new PDO(
+            $params['dsn'],
+            $params['user'],
+            $params['password'],
+            $params['options']
+        );
+        
+        $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+        $this->connections[$name] = $con;
+    }
+    
+    public function getConnection($name = null)
+    {
+        if(is_null($name)) {
+            return current($this->connections);
+        }
+        return $this->connections[$name];
+    }
+}
